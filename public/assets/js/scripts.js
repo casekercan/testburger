439204e47051885bd1cbf92e2259532f15dc9052
casekercan <casekercan@gmail.com>
2019-02-20 00:22:08 -0800

    try again

--- public/assets/js/scripts.js
+++ public/assets/js/scripts.js
@@ -14,7 +14,7 @@ $(document).ready(function () {
         );
     });
 
-    $(".create-form").on("submit", function (event) {
+    $(".create-update-form").on("submit", function (event) {
         // Make sure to preventDefault on a submit event.
         event.preventDefault();
 
